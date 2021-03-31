b209cdc6df36de713f500f1fd5c4de5dc0f85857
tamu-phys <blahbalh>
2021-03-31 15:14:13 -0500

    working version

--- auto_start_gphoto.py
+++ auto_start_gphoto.py
@@ -5,14 +5,16 @@ import os
 import numpy as np 
 import subprocess
 import io
+import subprocess
 
 #initilize video loopback dev
-n_dev = 10
+n_dev = 5
 start = 10
 dev_list = np.arange(n_dev) + start
 dev_list = str(dev_list)[1:-1].replace(" ",",")
 modprobe_command = "sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2 devices={} video_nr={}".format(n_dev,dev_list)
-os.system(modprobe_command)
+print(modprobe_command)
+#os.system(modprobe_command)
 
 def get_text_command(cmd): 
     return subprocess.check_output(cmd, shell=True).decode('utf-8')
@@ -27,15 +29,36 @@ def get_owner_name(usb):
         if "Current: " in out:
             return out.replace("Current: ", "")
 
-def get_camera_list():
+def get_camera_dict():
     result = get_text_command("gphoto2 --auto-detect")
     camera_list = result.split("\n")
     camera_list = camera_list[2:-1]
     usb_list = list(map(get_usb_num, camera_list))
-    ownername_list = []
-    
+    ownername_dict = {}    
     for port in usb_list:
-        print(get_owner_name(port))
-get_camera_list()
+        ownername_dict[get_owner_name(port)] = port
+    return ownername_dict
 
+start_video = "gphoto2 --stdout --capture-movie --port=usb:{} | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 {}"
+
+def run_camera(port,dev):
+    os.system(start_video.format(port,dev))
+
+if __name__=="__main__":
+    device_map = {'demo1':'/dev/video10', 'demo2':'/dev/video11', 'studio1':'/dev/video12', 'studio2':'/dev/video13'}
+
+    camera_dict = get_camera_dict()
+    subprocess.call(['ls','-l','-a'],shell=True)
+    for camera in device_map:
+        port = camera_dict[camera]
+        dev = device_map[camera]
+        print(camera,port,dev)
+        start_video_command = start_video.format(port,dev)
+        print(start_video_command)
+        print(start_video_command.split(" "))
+        subprocess.Popen([start_video_command],shell=True)
+        #run_camera(port,dev)
+
+    #gphoto_command = start_video.format()
+    #os.system()
 
