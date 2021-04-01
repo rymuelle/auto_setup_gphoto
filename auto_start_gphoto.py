7a7fdde8e7251eb4577421c421612bd0e4c7d977
tamu-phys <blahbalh>
2021-04-01 09:43:36 -0500

    working terminate signal

--- auto_start_gphoto.py
+++ auto_start_gphoto.py
@@ -7,25 +7,6 @@ import subprocess
 import signal
 from optparse import OptionParser
 
-
-
-parser = OptionParser()
-
-parser.add_option("-r", "--run",
-                  action="store_true", dest="run", default=False,
-                  help="run the gphoto commands")
-
-(options, args) = parser.parse_args()
-
-#initilize video loopback dev
-n_dev = 5
-start = 10
-dev_list = np.arange(n_dev) + start
-dev_list = str(dev_list)[1:-1].replace(" ",",")
-modprobe_command = "sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2 devices={} video_nr={}".format(n_dev,dev_list)
-print(modprobe_command)
-#os.system(modprobe_command)
-
 def get_text_command(cmd): 
     return subprocess.check_output(cmd, shell=True).decode('utf-8')
 
@@ -34,6 +15,7 @@ def get_usb_num(gphoto2_str):
 
 def get_owner_name(usb): 
     output =  get_text_command("gphoto2 --get-config ownername --port=usb:{}".format(usb))
+    print(output)
     output = output.split("\n")
     for out in output:
         if "Current: " in out:
@@ -41,6 +23,7 @@ def get_owner_name(usb):
 
 def get_camera_dict():
     result = get_text_command("gphoto2 --auto-detect")
+    print(result)
     camera_list = result.split("\n")
     camera_list = camera_list[2:-1]
     usb_list = list(map(get_usb_num, camera_list))
@@ -52,27 +35,47 @@ def get_camera_dict():
             print("port {} in use".format(port))
     return ownername_dict
 
-start_video = "gphoto2 --stdout --capture-movie --port=usb:{} | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 {}"
+start_video = "/usr/bin/gphoto2 --stdout --capture-movie --port=usb:{}"
 
-def run_camera(port,dev):
-    os.system(start_video.format(port,dev))
+send_to_dev = "/usr/bin/ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 {}"
+
+device_map = {'demo1':'/dev/video10', 'demo2':'/dev/video11', 'studio1':'/dev/video12', 'studio2':'/dev/video13'}
 
 if __name__=="__main__":
-    device_map = {'demo1':'/dev/video10', 'demo2':'/dev/video11', 'studio1':'/dev/video12', 'studio2':'/dev/video13'}
+    parser = OptionParser()
+    parser.add_option("-r", "--run",
+                  action="store_true", dest="run", default=False,
+                  help="run the gphoto commands")
+    (options, args) = parser.parse_args()
 
     camera_dict = get_camera_dict()
-    processes = []
-    with open("./setup_cams.sh", 'w') as f:
-        f.write('#!/bin/bash\n')
-        for camera in camera_dict:
-            port, dev = camera_dict[camera], device_map[camera]
-            start_video_command = start_video.format(port,dev)
-            print(start_video_command)
-            if options.run: processes.append(subprocess.Popen([start_video_command],shell=True))
+    dev_process = []
+    video_process = []
 
-input("Press Enter to continue...")
+    for camera in camera_dict:
+        port, dev = camera_dict[camera], device_map[camera]
+        start_video_command = start_video.format(port,dev)
+        send_to_dev_command = send_to_dev.format(dev)
+        print("===============")
+        print(start_video_command)
+        print(send_to_dev_command)
+        if options.run:
+            video_popen = subprocess.Popen(
+                start_video_command.split(" "),
+                stdout=subprocess.PIPE)
+            dev_popen = subprocess.Popen(
+                send_to_dev_command.split(" "),
+                stdin=video_popen.stdout,
+                stdout=subprocess.PIPE)
+            dev_process.append(dev_popen)
+            video_process.append(video_popen)
+                
+input("Press Enter to reset cameras...")
 
-for proc in processes:
+for v,d in zip(video_process,dev_process):
     print("killed")
-    proc.kill()
+    v.stdout.close()
+    d.terminate()
+    v.terminate()
+    
 
